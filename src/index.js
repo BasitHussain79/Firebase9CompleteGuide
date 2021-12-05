5cf66c12338aa98a5aa8342833ffa9b4e6b2dbde
BasitHussain79 <basithussainqadri@gmail.com>
2021-12-05 20:48:08 +0500

    document add,  delete, relatime data and queries

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import {initializeApp} from 'firebase/app';
-import {getFirestore, addDoc, collection, getDocs} from 'firebase/firestore';
+import {getFirestore, addDoc, collection, getDocs, doc, deleteDoc, onSnapshot, query, where, orderBy, serverTimestamp} from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyB4IygtpnAR1mKaxFQ90w_HteOHSTjWav4",
@@ -17,28 +17,60 @@ const db = getFirestore();
 
 const _collection = collection(db, 'students');
 
-// add Doc
-addDoc(_collection, {
-        name: 'Subhan',
-        depratment: 'CS',
+// get Doc
+// getDocs(_collection)
+//     .then((snapshot) => {
+//         let student = [];
+//         snapshot.forEach((snap) => {
+//             console.log(snap.id, snap.data());
+//             student.push({id: snap.id, ...snap.data() })
+//             console.log(student);
+//         })
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     });
+
+const _query = query(_collection, where('name', '==', 'Huzaifa'), orderBy('tech', 'desc'));
+
+onSnapshot(_query, (snapshot) => {
+    let student = [];
+    snapshot.forEach((doc) => {
+        console.log(doc.id, doc.data());
+        student.push({id: doc.id, ...doc.data()});
+        console.log(student);
+    })
 })
-.then(() => {
-        console.log('Document Added');
-})
-.catch(err => {
-        console.log(err)
+
+
+const addStudentForm = document.querySelector('.addStudent');
+addStudentForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addDoc(_collection, {
+        name: addStudentForm.name.value,
+        tech: addStudentForm.tech.value,
+        timestamp: serverTimestamp(),
+    })
+    .then(() => {
+        addStudentForm.reset();
+        console.log('Student Added');
+    })
+    .catch(err => {
+        console.log(err);
+    })
 });
 
 
-// get Doc
-getDocs(_collection)
-    .then((snapshot) => {
-        snapshot.forEach((snap) => {
-            console.log(snap.data());
-        })
+const deleteStudentForm = document.querySelector('.removeStudent');
+deleteStudentForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const docRef = doc(db, 'students', deleteStudentForm.id.value);
+    deleteDoc(docRef)
+    .then(() => {
+        deleteStudentForm.reset();
+        console.log('Student removed!');
     })
     .catch((err) => {
-        console.log(err)
+        console.log(err);
     });
-
-
+})
